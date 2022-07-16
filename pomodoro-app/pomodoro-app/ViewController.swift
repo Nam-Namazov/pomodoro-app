a7374dcdbf85f677d3c31c52bed93e11604d8575
Namik <namazov306@gmail.com>
2022-07-16 16:53:19 +0300

    project init

---
+++ pomodoro-app/pomodoro-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  pomodoro-app
+//
+//  Created by Намик on 7/16/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
